14a0ead6e755e01b71373200ba43fae7fa98df93
Pavel Osharin <mr.osharin@mail.ru>
2023-09-07 14:00:45 +0300

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public int calculate (int period, int summ, double percent) {
-    double percentInMonth = (percent / 12 / 100);
-    int month = (period * 12);
-    double summMonth = (summ * percentInMonth * (Math.pow(1 + percentInMonth, month))) / (Math.pow(1 + percentInMonth, month) - 1);
-    return (int) summMonth;
+        double percentInMonth = (percent / 12 / 100);
+        int month = (period * 12);
+        double summMonth = (summ * percentInMonth * (Math.pow(1 + percentInMonth, month))) / (Math.pow(1 + percentInMonth, month) - 1);
+        return (int) summMonth;
     }
 }
